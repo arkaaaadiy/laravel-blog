da00843f7650716c0eef0e15390dcaaa553cf0c6
arkaaaadiy <42414769+arkaaaadiy@users.noreply.github.com>
2019-07-25 22:03:18 +0500

    Стилизация

--- resources/views/blog/article.blade.php
+++ resources/views/blog/article.blade.php
@@ -13,7 +13,11 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-12">
+            <small class="text-muted">Опубликованно {{$article->created_at->format('H:i d.m.Y')}}</small>
             <h1>{{$article->title}}</h1>
+            @foreach ($article->downloads as $download)
+            <img src="{{asset('/storage/'.$download->path)}}" class=" w-50 p-3 img-fluid shadow-sm p-3 mb-5 bg-white rounded" alt="...">
+            @endforeach
             <p>{!! $article->description !!}</p>
         </div>
     </div>
--- resources/views/blog/home.blade.php
+++ resources/views/blog/home.blade.php
@@ -4,17 +4,17 @@
 <div class="container">
     <div class="row">
         @foreach ($articles as $article)
-        <div class="col-sm-3">
+        <div class="col-sm-4">
             <div class="card">
                 @foreach ($article->downloads as $download)
                 <img src="{{asset('/storage/'.$download->path)}}" class="card-img-top" alt="...">
                 @endforeach
                 <div class="card-body">
-                    <h5 class="card-title">{{$article->title}}</h5>
+                    <h5 class="card-title"><a href="{{route('article', $article->slug)}}">{{$article->title}}</a></h5>
                     <p class="card-text">{{$article->disciption_short}}</p>
                 </div>
                 <div class="card-footer">
-                    <small class="text-muted">{{$article->created_at}}</small>
+                    <small class="text-muted">{{$article->created_at->format('H:i d.m.Y')}}</small>
                 </div>
             </div>
         </div>
